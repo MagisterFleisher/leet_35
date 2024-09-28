c7df0ac49f2d5d2c4f58460e6fb856e338c340cc
Aaron Fleisher <amfleish@gmail.com>
2024-09-28 12:40:07 -0700

    finally works

--- src/include/binary_locate.c
+++ src/include/binary_locate.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -10,80 +8,56 @@ void temp(void) {
   return;
 }
 
-
 int binaryLocate(int* nums, int numsSize, int target) {
-  int found_index = -1;
-  
-  enum search_enum {
-    EQUAL_TO_MIDDLE = 1,
-    GREATER_THAN_MIDDLE = 2,
-    LESS_THAN_MIDDLE = 3
+  assert(0 < numsSize );
+  enum position_truth {
+    AT_MIDDLE = 1,
+    AT_GREATER = 2,
+    AT_LESS = 3
   };
-
-  (void) assert(0 <= numsSize);
-  bool continue_searching = true;
-  int_fast16_t start_index = 0;
-  int_fast16_t end_index = numsSize - 1;
-  int_fast8_t case_search_size = (end_index - start_index);
-  int_fast16_t middle_index = numsSize / 2;  
-  int_fast8_t find_area = (target == nums[middle_index]) + (2*(target > nums[middle_index])) + (3*(target < nums[middle_index]));
-
-  while(continue_searching) {
-    if(case_search_size == 0) {
-        /*
-          1 VALUE IN ARRAY 
-        */
-        if(target == nums[middle_index]) {
-          (void) printf("\n%d-\ttarget %d == nums[middle_index] %d\tmiddle_index: %d\n", __LINE__, target, nums[middle_index], middle_index);
-          return middle_index;
-        }
-        if(target < nums[middle_index]) {
-          (void) printf("\n%d\ttarget %d < nums[middle_index] %d\tmiddle_index: %d\n", __LINE__, target, nums[middle_index], middle_index);
-          return middle_index;
-        }
-        if(target > nums[middle_index]) {
-          (void) printf("\n%d\ttarget %d > nums[middle_index] %d\tmiddle_index: %d\n", __LINE__, target, nums[middle_index], middle_index);
-          return (middle_index + 1);
-        }
+  int target_index = 0;
+  if(0 == numsSize) {
+    if(target > nums[0]) {
+      return 1;
     } else {
-        /*
-          MORE THAN 1 VALUES IN ARRAY
-        */
-        /* Find area
-          1 -> the search value is at the middle value
-          2 -> the search value is greater than the  middle value
-          3 -> the search value is less than the middle value
-          default -> this shouldn't happen
-        */
-        switch(find_area) {
-          case EQUAL_TO_MIDDLE : {
-            (void) printf("\n%d-\ttarget: %d\tfound at middle index: %jd\tmiddle index value: %d\n", __LINE__, target, middle_index, nums[middle_index]);
-            return middle_index;
-            //found_index = middle_index;
-            continue_searching = false;
-            break;
-          };
-          case GREATER_THAN_MIDDLE : {
-            (void) printf("%d-\tgreater than middle index: %ju\tmiddle index value: %d\n", __LINE__, middle_index, nums[middle_index]);
-            start_index = middle_index + 1;
-            case_search_size = end_index - start_index;
-            middle_index = (end_index - start_index) + middle_index;
-            break;
-          };
-          case LESS_THAN_MIDDLE : {
-            (void) printf("%d-\tless that middle index: %ju\tmiddle index value: %d\n", __LINE__, middle_index, nums[middle_index]);
-            end_index = middle_index - 1;
-            case_search_size = end_index - start_index;
-            middle_index = (end_index - start_index) + start_index;
-            break;
-          };
-          default : {
-            break;
-          };
-        }
-      }
-      find_area = (target == nums[middle_index]) + (2*(target > nums[middle_index])) + (3*(target < nums[middle_index]));
-      case_search_size = end_index - start_index;
+      return 0;
     }
-  return found_index;
+  } else {
+    if(target <= nums[0]) { return 0;}
+    uint16_t start_index = 0;
+    uint16_t end_index = (numsSize - 1);
+    uint16_t search_space = end_index - start_index;
+    uint16_t middle_index = start_index + (search_space / 2);
+    uint8_t switch_case = (target == nums[middle_index]) + (2 * (target > nums[middle_index]) + (3 * (target < nums[middle_index])));
+    bool continue_searching = true;
+    while(continue_searching) {
+      switch(switch_case) {
+        case AT_MIDDLE : {
+          return middle_index;
+          break; };
+        case AT_GREATER : { 
+          if(middle_index < end_index) {
+            start_index = middle_index + 1;
+          } else {
+            return (middle_index + 1);
+          }
+          break; };
+        case AT_LESS : { 
+          if((start_index < middle_index) && (start_index < end_index)) {
+            end_index = middle_index - 1;
+          } else {
+            return middle_index;
+          }
+          break; };
+        default :  { 
+          // shrug
+          return middle_index;
+          break; };
+      }
+      search_space = end_index - start_index;
+      middle_index = start_index + (search_space / 2);
+      switch_case = (target == nums[middle_index]) + (2 * (target > nums[middle_index]) + (3 * (target < nums[middle_index])));
+      }
+  }
+  return target_index; 
 }
--- src/main.c
+++ src/main.c
@@ -10,24 +10,31 @@ int main(int argument_count, char** argument_vector) {
   int array0[1] = { 0 };
   int array1[2] = { -1, 0 };
   int array2[2] = { 5, 8};
-  int array4[3] = { 0, 1, 6  };
-  int array5[3] = { -1, 0, 7 };
-  int array6[4] = { -1, 1, 6  };
-  int array7[4] = { -1, 0, 1, 6  };
-  int array8[6] = { -2, -1, 0, 2, 7};
-  int array9[6] = { -2, -1, 1, 2, 7};
-  int array10[7] = { -2, -1, 0, 1, 2, 7};
-  int array11[7] = { -2, -1, 1, 2, 7, 8};
+  int array3[3] = { 0, 1, 6  };
+  int array4[3] = { -1, 0, 7 };
+  int array5[4] = { -1, 1, 6  };
+  int array6[4] = { -1, 0, 1, 6  };
+  int array7[5] = { -2, -1, 0, 2, 7};
+  int array8[5] = { -2, -1, 1, 2, 7};
+  int array9[6] = { -2, -1, 0, 1, 2, 7};
+  int array10[6] = { -2, -1, 1, 2, 7, 8};
+  int array11[7] = { -2, -1, 1, 2, 7, 8, 15};
 
-  int *nums[] = { array0, array1, array2, array4, 
+  int *nums[] = { array0, array1, array2, array3, array4, 
     array5, array6, array7, array8, array9, array10, array11 };
-  int numsSize[11] = { 1, 2, 2, 3, 3, 4, 4, 6, 6, 7, 7 };
+  int numsSize[12] = { 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7 };
 
   int target[4] = { 0, 1, 5, 11 };
-  int target0 = 11;
 
-  for(uint_fast8_t index0 = 0; index0 < 11; index0++) {
-      printf("array%d\tindex: %d\n", index0, binaryLocate(nums[index0], numsSize[index0], target0)); }
+  int target0 = 3;
+
+  for(uint_fast8_t index0 = 0; index0 < 12; index0++) {
+    for(uint_fast8_t index1 = 0; index1 < 4; index1++) {
+      printf("array%d\tnumsSize: %d\ttarget: %d\tindex: %d\n", index0, numsSize[index0], target[index1], binaryLocate(nums[index0], numsSize[index0], target[index1])); 
+      }
+  }
+  
+//  printf("\n\narray%d\tnumsSize: %d\ttarget: %d\nindex: %d\n\n\n", 5, numsSize[5], target0, binaryLocate(nums[5], numsSize[5], target0)); 
+  
   return error;
-
 }
